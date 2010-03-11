6aececf20b088ab1fc8e805f984898c71dde0845
Robbie Trencheny <me@robbiet.us>
2010-03-11 09:55:30 -0800

    added in jQuery and a blank style.css

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <html>
 <head>
 <title>Statusarchy</title>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
+<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
 </head>
 <body>
 <h1>Future home of Statusarchy!</h1>
