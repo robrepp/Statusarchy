496176e6f05880dd86a1b6144c026526059ce6b6
Ryan Sullivan <ryansully@Swine.local>
2010-03-17 16:05:19 -0400

    Moved themes folder to WWW, added jQuery from Google and added Main JS file (Since the JS isn't going to be on a per-theme basis I assume)

--- www/index.php
+++ www/index.php
@@ -1,11 +1,18 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>Statusarchy</title>
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
-<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>StatusArchy</title>
+	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	
 </head>
+
 <body>
-<h1>Future home of Statusarchy!</h1>
+
+
 </body>
 </html>
-
