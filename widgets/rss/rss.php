e0ca16ac5da082fe7cc184a540b78d9fed8c53c3
Ryan Sullivan <ryansully@Swine.local>
2010-03-19 14:49:26 -0400

    Fixing up the RSS function in general, although SimplePie isn't anywhere to be found?
    I'd like to keep everything using a specific PHP format. Using single quotes (except for MySQL queries), use dirname(__FILE__) for requires, includes, etc (keep things structured), keep functions indented, don't use old style foreach ():, foreach() { } or foreach() (for one line) will do the trick. Just how I prefer to code, we can discuss this anytime, I'm reasonable!

--- widgets/rss/rss.php
+++ widgets/rss/rss.php
@@ -1,12 +1,16 @@
 <?php
-function rss($url) {
-require_once('SimplePie/simplepie.class.php');
-$feed = new SimplePie();
-$feed->set_feed_url($url);
-$feed->handle_content_type();
+require_once dirname(__FILE__) . '/SimplePie/simplepie.class.php';
 
-echo "<h2>" . $feed->get_title() . "<br>";
-foreach ($feed->get_items() as $item):
-echo $item->get_title();
+function rss($url)
+{
+	$feed = new SimplePie();
+	$feed->set_feed_url($url);
+	$feed->handle_content_type();
+	
+	// Display content:
+	echo '<h2>' . $feed->get_title() . '<br />';
+	
+	foreach($feed->get_items() as $item)
+		echo $item->get_title();
 }
 ?>
