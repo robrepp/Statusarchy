615d4b5a5719378e602e70625ff53ac70649a889
Ryan Sullivan <ryansully@Swine.local>
2010-03-22 22:17:55 -0400

    Finishing up the JS for now, more to come soon

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
 	$('.w3standards').remove(); // These are just to get things validated
+	
+	// Setup sorting/drag+drop
 	$('#modules').sortable().disableSelection();
+	$('#possibleModules li').draggable({
+		connectToSortable: 'ul#modules',
+		helper: 'clone',
+		revert: 'invalid'
+	}).disableSelection();
+	
+	// Setup fadein/out for top bar
+	$('#adder').width($('#possibleModules').width()).height($('#possibleModules').height()).hover(function(){
+		$('#possibleModules').fadeIn();
+	}, function(){
+		$('#possibleModules').fadeOut();
+	});
+	$('#possibleModules').fadeOut();
 });
